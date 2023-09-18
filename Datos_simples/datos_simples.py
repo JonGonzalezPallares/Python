4b7c43e6a81cfa573f2f689436ccbe68296befc7
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2023-09-18 09:41:41 +0200

    Create datos_simples.py

---
+++ Datos_simples/datos_simples.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+print("Ejercicios de datos simples")
+
+'''
+print("Ejercicio 1")
+print("¡Hola Mundo!")
+
+print("\nEjercicio 2")
+var = "¡Hola Mundo!"
+print(var)
+
+print("\nEjercicio 3")
+nombre = input("Introduce tu nombre: ")
+print("¡Hola {}!", nombre)
+
+print("\nEjercicio 4")
+ecuacion = ((3+2)/(2*5))**2
+print(ecuacion)
+
+print("\nEjercicio 5")
+horas = input("Horas trabajadas: ")
+coste = input("Coste por hora: ")
+total = float(horas)*float(coste)
+print("Paga: ", total)
+
+print("\nEjercicio 6")
+entero = int(input("Introduce un numero positivo: "))
+if isinstance(entero, int):
+    if(entero>0):
+        suma = (entero*(entero+1))/2
+        print ("La suma es: ", suma)
+    else:
+        print("El numero no es positivo")
+
+print("\nEjercicio 7")
+peso = float(input("Introduce tu peso (en Kg): "))
+estatura = float(input("Introduce tu estatura (en m): "))
+imc = peso/(estatura**2)
+print("Tu indice de masa corporal es: ", imc)
+
+print("\Ejercicio 8")
+entero1 = int(input("Introduce un numero entero: "))
+entero2 = int(input("Introduce otro numero entero: "))
+print(entero1, " dividido por ", entero2, " da un cociente de ", str(entero1//entero2))
+print(entero1, " dividido por ", entero2, " da un resto de ", str(entero1 % entero2))
+
+print("\nEjercicio 9")
+invertir = float(input("Introduce una cantidad a invertir: "))
+interes = float(input("Introduce el interes anual: "))
+años = float(input("Introduce el numero de años: "))
+capital = round(invertir * (interes/100+1) ** años, 2)
+print("El capital final es de ", capital)
+
+print("\nEjercicio 10")
+payasos = 112
+muñeca = 75
+vendidosP = int(input("Cantidad de payasos vendidos: "))
+vendidosM = int(input("Cantidad de muñecas vendidas: "))
+pesoT = ((payasos*vendidosP)+(muñeca*vendidosM))
+print("Peso del paquete es de: ", pesoT)
+
+print("\nEjercicio 11")
+cantidad = float(input("Introduce la cantidad del banco: "))
+año1 = round((cantidad+(cantidad*0.04)), 2)
+año2 = round((año1+(año1*0.04)), 2)
+año3 = round((año2+(año2*0.04)), 2)
+print("Ahorro tras el primer año: ", año1)
+print("Ahorro tras el segundo año: ", año2)
+print("Ahorro tras el tercer año: ", año3 )
+
+print("\nEjercicio 12")
+barras = 3.49
+descuento = 0.6
+vendidas = int(input("Cantidad de barras de pan no del dia vendidas: "))
+total = round(((barras*descuento)*vendidas), 2)
+print("El precio de las barras es de ", barras, " tiene un descuento de ", descuento, " y el precio final con descuento es de: ", total)
+'''
